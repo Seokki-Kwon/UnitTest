36ca5efad7e60d42833c28a58a06d91f307c6940
Seokki-Kwon <seokki0706@gmail.com>
2024-07-13 01:39:21 +0900

    Initial Commit

---
+++ UnitTest/UnitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitTestApp.swift
+//  UnitTest
+//
+//  Created by 석기권 on 7/13/24.
+//
+
+import SwiftUI
+
+@main
+struct UnitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
