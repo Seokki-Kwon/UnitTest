56dfc0ce663edf6dcbd5abecf292237fcee44575
Seokki-Kwon <seokki0706@gmail.com>
2024-07-13 02:40:03 +0900

    add test

--- UnitTest/ContentView.swift
+++ UnitTest/ContentView.swift
@@ -6,14 +6,82 @@
 //
 
 import SwiftUI
+import Combine
+
+struct TestModel: Identifiable {
+    let id = UUID().uuidString
+    let name: String
+}
+
+class TestDataService {
+    private let data = [TestModel(name: "1"),
+                TestModel(name: "2"),
+                TestModel(name: "3")]
+    
+    func fetchData() -> AnyPublisher<[TestModel], Never> {
+        return Just(data)
+            .delay(for: 2, scheduler: DispatchQueue.main)
+            .eraseToAnyPublisher()
+    }
+}
+
+enum TestError: Error {
+    case rangeError
+    
+    var errorType: Int {
+        switch self {
+        case .rangeError:
+            0
+        }
+    }
+}
+
+class TestViewModel: ObservableObject {
+    @Published var models: [TestModel] = []
+    
+    let dataService: TestDataService
+
+    var cancellable: AnyCancellable?
+    
+    init(_ dataService: TestDataService) {
+        self.dataService = dataService
+        
+       cancellable = dataService.fetchData()
+            .assign(to: \.models, on: self)
+    }
+    
+    func addData(_ model: TestModel) {
+        models.append(model)
+    }
+    
+    func removeData(at index: Int) throws {
+        if index >= models.count {
+            throw TestError.rangeError
+        }
+        models.remove(at: index)
+    }
+    
+    func removeAllData() {
+        models.removeAll()
+    }
+}
 
 struct ContentView: View {
+    @StateObject var viewModel = TestViewModel(TestDataService())
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            List(viewModel.models) {
+                Text($0.name)
+            }
+            
+            Button(action: {}, label: {
+                Text("Add Model")
+            })
+            
+            Button(action: {}, label: {
+                Text("Delete Model")
+            })
         }
         .padding()
     }
--- UnitTestTests/UnitTestTests.swift
+++ UnitTestTests/UnitTestTests.swift
@@ -5,25 +5,66 @@
 //  Created by 석기권 on 7/13/24.
 //
 
+/*:
+ ## Test Structure
+ - TestMethod: 코드의 일부를 검증하는 메서드 통과 또는 실패 결과를 생성
+ - TestClass: TestMethod의 집합 일반적으로 테스트할 로직을 그룹화(ex: 인증)
+ - TextBundle: 테스트 클래스 그룹을 포함하며 UnitTest 또는 UITest 유형중 하나이다
+ - TestPlan: TestBundle의 그룹이며 테스트 계획에서 테스트를 실행할 때 고려해야 할 구성 목록을 설정?
+ 
+XCTest
+Apple에서 제공하는 Test Framework
+ 
+XCTestCase
+단위 테스트를 작성하고 실행하는데 필요한 모든 작업을 제공하는 클래스
+
+ ## Test Pattern
+ Arrange / Act / Assert
+ Arrange: 테스트에 사용되는 객체 또는 데이터를 생성
+ Act: 메서드나 함수를 실행
+ Assert: 테스트 결과를 비교
+ */
 import XCTest
 @testable import UnitTest
 
 final class UnitTestTests: XCTestCase {
-
+    private var viewModel: TestViewModel!
+    
+    // 테스트 하기전에 실행되는 메서드
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        viewModel = TestViewModel(TestDataService())
     }
 
+    // 테스트가 종료되고 정리 메서드
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        viewModel.removeAllData()
+        viewModel = nil
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    
+    // 모델 추가 테스트
+    func testAddNewModel() throws {
+        // Arrange
+        let newModel = TestModel(name: "1")
+        
+        // Act
+        viewModel.addData(newModel)
+        
+        // Assert
+        XCTAssertEqual(viewModel.models.count, 1) // 2개의 비교결과 검증
+    }
+    
+    func testRemoveModel() throws {
+        let newModel = TestModel(name: "1")
+        let newModel2 = TestModel(name: "2")
+        
+        viewModel.addData(newModel)
+        viewModel.addData(newModel2)
+        
+        // removeData 메서드가 thorw 하는경우
+        // Error를 throw하지 않는 경우도 테스트 실패
+        XCTAssertThrowsError(try viewModel.removeData(at: 1)) { error in
+           XCTFail("Error")
+        }
     }
 
     func testPerformanceExample() throws {
